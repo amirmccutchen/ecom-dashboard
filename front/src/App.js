aee079ce2d6f76119e58491f91b9e1d2412195f3
Amir McCutchen <3amproduced@gmail.com>
2023-01-27 12:55:50 -0500

    set up material ui in appjs

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
 
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
+};
 
 export default App;
