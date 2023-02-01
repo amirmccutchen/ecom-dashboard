d1fbf00c27d821aead92673b58b067f024ba375a
Amir McCutchen <3amproduced@gmail.com>
2023-02-01 14:28:33 -0500

    added cust route + getcustquery + cust page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from '../models/Product.js';
 import ProductStats from '../models/ProductStats.js';
+import User from '../models/User.js';
 
 export const getProducts = async (req, res ) => {
     try {
@@ -15,6 +16,15 @@ export const getProducts = async (req, res ) => {
             })
         );
         res.status(200).json(productsStats);
+      } catch (error) {
+        res.status(404).json({ message: error.message });
+      };
+}
+
+export const getCustomers = async (req, res) => {
+    try {
+        const customers = await User.find({ role: 'user' }).select('-password');
+        res.status(200).json(customers);
       } catch (error) {
         res.status(404).json({ message: error.message });
       };
--- webpage/src/App.js
+++ webpage/src/App.js
@@ -7,12 +7,13 @@ import { BrowserRouter, Routes , Route, Navigate } from 'react-router-dom';
 import Layout from './scenes/layout';
 import Dashboard from './scenes/dashboard';
 import Products from 'scenes/products';
+import Customers from 'scenes/customers';
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className='app'>
+    <div className = 'app'>
       <BrowserRouter>
         <ThemeProvider theme = {theme}>
           <CssBaseline />
@@ -20,7 +21,8 @@ const App = () => {
             <Route element = {<Layout />}>
               <Route path = '/' element = {<Navigate to = '/dashboard' replace />} />
               <Route path = '/dashboard' element = {<Dashboard />} />
-              <Route path = '/products' element = {<Products />} />
+              <Route path = '/products'  element = {<Products /> } />
+              <Route path = '/customers' element = {<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
