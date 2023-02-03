757581e22c8abe96b6271bdd8637da91bfb9de45
Amir McCutchen <3amproduced@gmail.com>
2023-02-03 16:20:27 -0500

    completed dashboard home page

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -3,28 +3,35 @@ import OverallStats from "../models/OverallStats.js";
 import Transaction from "../models/Transaction.js";
 
 export const getUser = async (req, res) => {
+  
   try {
+
     const { id } = req.params;
     const user = await User.findById(id);
+
     res.status(200).json(user);
+
   } catch (error) {
+
     res.status(404).json({ message: error.message });
+
   };
 };
 
+
+
 export const getDashboardStats = async (req, res) => {
+
   try {
-    // hardcoded values
+
     const currentMonth = "November";
     const currentYear = 2021;
     const currentDay = "2021-11-15";
 
-    /* Recent Transactions */
     const transactions = await Transaction.find()
       .limit(50)
       .sort({ createdOn: -1 });
 
-    /* Overall Stats */
     const overallStat = await OverallStats.find({ year: currentYear });
 
     const {
@@ -53,7 +60,10 @@ export const getDashboardStats = async (req, res) => {
       todayStats,
       transactions,
     });
+
   } catch (error) {
+
     res.status(404).json({ message: error.message });
-  }
+
+  };
 };
