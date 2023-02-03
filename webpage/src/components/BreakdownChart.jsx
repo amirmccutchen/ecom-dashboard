55455e5ce9fd4ff15627c6f123c399551c69102c
Amir McCutchen <3amproduced@gmail.com>
2023-02-03 13:00:22 -0500

    created admin route and controller

--- webpage/src/components/BreakdownChart.jsx
+++ webpage/src/components/BreakdownChart.jsx
@@ -26,15 +26,15 @@ const BreakdownChart = ({ isDashboard = false }) => {
 
   return (
     <Box
-      height={isDashboard ? '400px' : '100%'}
-      width={undefined}
-      minHeight={isDashboard ? '325px' : undefined}
-      minWidth={isDashboard ? '325px' : undefined}
-      position='relative'
+      height = {isDashboard ? '400px' : '100%'}
+      width = {undefined}
+      minHeight = {isDashboard ? '325px' : undefined}
+      minWidth = {isDashboard ? '325px' : undefined}
+      position = 'relative'
     >
       <ResponsivePie
-        data={formattedData}
-        theme={{
+        data = {formattedData}
+        theme = {{
           axis: {
             domain: {
               line: {
@@ -67,26 +67,26 @@ const BreakdownChart = ({ isDashboard = false }) => {
             },
           },
         }}
-        colors={{ datum: 'data.color' }}
-        margin={
+        colors = {{ datum: 'data.color' }}
+        margin = {
           isDashboard
             ? { top: 40, right: 80, bottom: 100, left: 50 }
             : { top: 40, right: 80, bottom: 80, left: 80 }
         }
-        sortByValue={true}
-        innerRadius={0.45}
-        activeOuterRadiusOffset={8}
-        borderWidth={1}
-        borderColor={{
+        sortByValue = {true}
+        innerRadius = {0.45}
+        activeOuterRadiusOffset = {8}
+        borderWidth = {1}
+        borderColor = {{
           from: 'color',
           modifiers: [['darker', 0.2]],
         }}
-        enableArcLinkLabels={!isDashboard}
-        arcLinkLabelsTextColor={theme.palette.secondary[200]}
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{ from: 'color' }}
-        arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{
+        enableArcLinkLabels = {!isDashboard}
+        arcLinkLabelsTextColor = {theme.palette.secondary[200]}
+        arcLinkLabelsThickness = {2}
+        arcLinkLabelsColor = {{ from: 'color' }}
+        arcLabelsSkipAngle = {10}
+        arcLabelsTextColor = {{
           from: 'color',
           modifiers: [['darker', 2]],
         }}
@@ -117,13 +117,13 @@ const BreakdownChart = ({ isDashboard = false }) => {
         ]}
       />
       <Box
-        position='absolute'
-        top='50%'
-        left='50%'
-        color={theme.palette.secondary[400]}
-        textAlign='center'
-        pointerEvents='none'
-        sx={{
+        position = 'absolute'
+        top = '50%'
+        left = '50%'
+        color = {theme.palette.secondary[400]}
+        textAlign = 'center'
+        pointerEvents = 'none'
+        sx = {{
           transform: isDashboard
             ? 'translate(-75%, -170%)'
             : 'translate(-50%, -100%)',
